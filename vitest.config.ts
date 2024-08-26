573adc4abec3019fa2118096a1d57aa77cf535cc
Trey Crossley <treycrossley@gmail.com>
2024-08-26 14:59:43 -0700

    vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,11 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    globals: true,  // Enables global functions like describe, it, etc.
-    environment: 'jsdom',  // Set the test environment to jsdom
     coverage: {
-      reporter: ['text', 'json-summary', 'json'], // Include the desired reporters
-      // Other coverage options can be configured here
-    },
+      reporter: ['text', 'json-summary', 'json'],
+      reportOnFailure: true,
+    }
   }
 });
