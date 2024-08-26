765f93c1a5622c431d91144f28da6b41f3f34d6c
Bo <bbonning42@gmail.com>
2024-08-26 14:37:20 -0500

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { BrowserRouter, Routes } from 'react-router-dom'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <App></App>
+    </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
