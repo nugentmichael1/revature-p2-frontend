085faa097f6a22f862f132bffb0e6ca2db3718f4
Bo <bbonning42@gmail.com>
2024-08-27 13:54:39 -0500

    register form

---
+++ src/pages/SignInRegisterPage/SignInRegisterPage.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+export default function SignInRegister() {
+    const [register, setRegister] = useState(true);
+
+    return (
+        <>
+        
+        </>
+    )
+}
