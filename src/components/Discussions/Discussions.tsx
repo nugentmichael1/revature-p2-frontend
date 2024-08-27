ba98ef69e422b77b7664b936dfae48858ec1d1e1
Josh Williams <j.williams219@outlook.com>
2024-08-27 15:02:07 -0600

    create discussions component

---
+++ src/components/Discussions/Discussions.tsx
@@ -0,0 +1,16 @@
+
+
+import { useState } from "react";
+
+
+export default function Discussions() {
+    const [discussions, setDiscussions] = useState([]);
+
+    return (<>
+        
+        <div>
+
+        </div>
+        
+        </>)
+}
