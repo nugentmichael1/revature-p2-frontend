23987c8fdbde760d5ce70d041f59f8ebf2bd50c6
Pichborith Kong <pichborith@gmail.com>
2024-08-27 13:23:09 -0700

    add home component to routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,22 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './index.css';
 import Layout from '../src/pages/Layout/layout';
+import Home from './components/Home/Home';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <BrowserRouter>
-          <Routes>
-            {/* <Route index element={<SignUp />}></Route> */}
-            <Route path="/" element={<Layout />}>
-              {/* <Route path="/home" element={<Home />}></Route>
+    <BrowserRouter>
+      <Routes>
+        {/* <Route index element={<SignUp />}></Route> */}
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="/home" element={<Home />}></Route>
               <Route path="/profile" element={<Profile />}></Route>
               <Route path="/transactions" element={<Transactions />}></Route> */}
-            </Route>
-          </Routes>
-      </BrowserRouter>
-  </StrictMode>
-)
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+);
