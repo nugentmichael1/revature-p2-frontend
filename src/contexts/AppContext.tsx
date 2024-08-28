31e9362af75de15c912703623e466a9479cac3b1
Bo <bbonning42@gmail.com>
2024-08-28 13:27:35 -0500

    add accounts to context

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -5,58 +5,71 @@ import {
     ReactNode,
     useEffect,
   } from "react";
-  import { User } from "../types/user";
+import { User } from "../types/user";
+import { Account } from "../types/account";
   
-  interface AppState {
-    user: User | null;
-    // Add additional state properties here
+interface AppState {
+  user: User | null;
+  accounts: Account[] | null;
+  // Add additional state properties here
+}
+
+interface AppContextType {
+  state: AppState;
+  setUser: (user: User | null) => void;
+  setAccounts: (accounts: Account[] | null) => void;
+  // Add additional state update functions here as needed
+}
+
+const AppContext = createContext<AppContextType | undefined>(undefined);
+
+export const useAppContext = () => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error("useAppContext must be used within an AppProvider");
   }
-  
-  interface AppContextType {
-    state: AppState;
-    setUser: (user: User | null) => void;
-    // Add additional state update functions here as needed
-  }
-  
-  const AppContext = createContext<AppContextType | undefined>(undefined);
-  
-  export const useAppContext = () => {
-    const context = useContext(AppContext);
-    if (!context) {
-      throw new Error("useAppContext must be used within an AppProvider");
+  return context;
+};
+
+// TODO: don't handle users in localStorage?
+export const AppProvider = ({ children }: { children: ReactNode }) => {
+  const [state, setState] = useState<AppState>(() => {
+    const storedUser = localStorage.getItem("user");
+    const storedAccounts = localStorage.getItem("accounts");
+    return {
+      user: storedUser ? JSON.parse(storedUser) : null,
+      accounts: storedAccounts ? JSON.parse(storedAccounts) : null,
+      // Initialize additional state properties here
+    };
+  });
+
+  useEffect(() => {
+    if (state.user) {
+      localStorage.setItem('user', JSON.stringify(state.user));
+    } else {
+      localStorage.removeItem('user');
     }
-    return context;
+
+    if (state.accounts) {
+      localStorage.setItem('accounts', JSON.stringify(state.accounts));
+    } else {
+      localStorage.removeItem('accounts');
+    }
+    // Handle additional state changes here
+  }, [state.user, state.accounts]);
+
+  const setUser = (user: User | null) => {
+    setState((prevState) => ({ ...prevState, user }));
   };
 
-  // TODO: don't handle users in localStorage?
-  export const AppProvider = ({ children }: { children: ReactNode }) => {
-    const [state, setState] = useState<AppState>(() => {
-      const storedUser = localStorage.getItem("user");
-      return {
-        user: storedUser ? JSON.parse(storedUser) : null,
-        // Initialize additional state properties here
-      };
-    });
-  
-    useEffect(() => {
-      if (state.user) {
-        localStorage.setItem('user', JSON.stringify(state.user));
-      } else {
-        localStorage.removeItem('user');
-      }
-      // Handle additional state changes here
-    }, [state.user]);
-  
-    const setUser = (user: User | null) => {
-      setState((prevState) => ({ ...prevState, user }));
-    };
-  
-    // Add additional state update functions here
-  
-    return (
-      <AppContext.Provider value={{ state, setUser }}>
-        {children}
-      </AppContext.Provider>
-    );
+  const setAccounts = (accounts: Account[] | null) => {
+    setState((prevState) => ({ ...prevState, accounts }));
   };
-  
+  // Add additional state update functions here
+
+  return (
+    <AppContext.Provider value={{ state, setUser, setAccounts }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
