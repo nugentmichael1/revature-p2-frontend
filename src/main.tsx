1e6f7342254f4573984d339a1d6a227be6a37ff0
Pichborith Kong <pichborith@gmail.com>
2024-08-28 13:25:38 -0700

    add course route to main

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import Layout from '../src/pages/Layout/layout';
 import Home from './components/Home/Home';
-import DashboardPage from "./pages/Dashboard/DashboardPage";
+import DashboardPage from './pages/Dashboard/DashboardPage';
+import AllCourses from './pages/AllCourses/AllCourses';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/dashboard' element={<DashboardPage />} />
+          <Route path='/courses' element={<AllCourses />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
