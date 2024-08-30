4352feab92efddcfc755393601dcf57660c13e20
Bo <bbonning42@gmail.com>
2024-08-30 11:26:02 -0500

    update register/signin page & temp logout

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,68 +1,157 @@
-import { useState } from "react";
-import PasswordStrengthMeter from "../PasswordStrengthMeter/PasswordStrengthMeter";
+import { useEffect, useState } from 'react';
+import PasswordStrengthMeter from '../PasswordStrengthMeter/PasswordStrengthMeter';
+import { useAppContext } from '../../contexts/AppContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function Register() {
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password1, setPassword1] = useState("");
-    const [password2, setPassword2] = useState("");
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password1, setPassword1] = useState('');
+  const [password2, setPassword2] = useState('');
+  const [role, setRole] = useState<'STUDENT' | 'EDUCATOR'>('STUDENT');
+  const {
+    state: { user },
+    setUser,
+  } = useAppContext();
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        if (password1 != password2) {
-            console.log('passwords do not match');
-        } else {
-            console.log(`username: ${username}, password: ${password1}`);
-        }
-    };
+  const nav = useNavigate();
 
-    const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUsername(e.target.value);
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (password1 != password2) {
+      console.log('passwords do not match');
+    } else if (role === "STUDENT" || role === "EDUCATOR"){
+      // TODO: axios request
+      setUser({
+        id: 0,
+        username: username,
+        email: email,
+        password: password1,
+        role: role,
+      });
+      console.log(`Registered as a ${role} with username: ${username}`);
+    } else {
+        console.log('Invalid role attempted to be sent.')
     }
+  };
 
-    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value);
-    };
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
 
-    const handlePassword1Change = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword1(e.target.value);
-    };
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
 
-    const handlePassword2Change = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword2(e.target.value);
-    };
+  const handlePassword1Change = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword1(e.target.value);
+  };
 
-    return (
+  const handlePassword2Change = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword2(e.target.value);
+  };
+
+  const handleRoleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value as 'STUDENT' | 'EDUCATOR';
+    if (value === 'STUDENT' || value === 'EDUCATOR') {
+      setRole(value);
+    } else {
+      console.log('Invalid role selected');
+    }
+  };
+
+  useEffect(() => {
+    if (user && user.username) {
+      nav('/dashboard');
+    }
+  }, [user, nav]);
+
+  return (
     <>
-        <div className="flex items-start justify-between p-5 border-b rounded-t">
-            <h3 className="text-xl font-semibold text-gray-900">
-                Sign Up
-            </h3>
-        </div>
+      <div className='flex items-start justify-between rounded-t border-b p-5'>
+        <h3 className='text-xl font-semibold text-gray-900'>Sign Up</h3>
+      </div>
 
-        <div className="p-6 space-y-6">
-            <form className="grid grid-cols-1 gap-6" onSubmit={handleSubmit}>
-                <div className="col-span-1">
-                    <label htmlFor="email" className="text-sm font-medium text-gray-900 block mb-2">Username</label>
-                    <input id="username" type="text" placeholder="Enter username..." required onChange={handleUsernameChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"/>
-                </div>
+      <div className='space-y-6 p-6'>
+        <form className='grid grid-cols-1 gap-6' onSubmit={handleSubmit}>
+          <div className='col-span-1'>
+            <label
+              htmlFor='email'
+              className='mb-2 block text-sm font-medium text-gray-900'
+            >
+              Username
+            </label>
+            <input
+              id='username'
+              type='text'
+              placeholder='Enter username...'
+              required
+              onChange={handleUsernameChange}
+              className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 shadow-sm focus:border-secondary-600 focus:ring-secondary-600 sm:text-sm'
+            />
+          </div>
 
-                <div className="col-span-1">
-                    <label htmlFor="password1" className="text-sm font-medium text-gray-900 block mb-2">Password</label>
-                    <input id="password1" type="password" required onChange={handlePassword1Change} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"/>
-                    <PasswordStrengthMeter password={password1} />
-                </div>
+          <div className='col-span-1'>
+            <label
+              htmlFor='password1'
+              className='mb-2 block text-sm font-medium text-gray-900'
+            >
+              Password
+            </label>
+            <input
+              id='password1'
+              type='password'
+              required
+              onChange={handlePassword1Change}
+              className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 shadow-sm focus:border-secondary-600 focus:ring-secondary-600 sm:text-sm'
+            />
+            <PasswordStrengthMeter password={password1} />
+          </div>
 
-                <div className="col-span-1">
-                    <label htmlFor="password2" className="text-sm font-medium text-gray-900 block mb-2">Confirm Password</label>
-                    <input id="password2" type="password" required onChange={handlePassword2Change} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"/>
-                </div>
+          <div className='col-span-1'>
+            <label
+              htmlFor='password2'
+              className='mb-2 block text-sm font-medium text-gray-900'
+            >
+              Confirm Password
+            </label>
+            <input
+              id='password2'
+              type='password'
+              required
+              onChange={handlePassword2Change}
+              className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 shadow-sm focus:border-secondary-600 focus:ring-secondary-600 sm:text-sm'
+            />
+          </div>
 
-                <div className="col-span-1">
-                    <button type="submit" className="text-white bg-secondary-500 hover:bg-secondary-700 focus:ring-4 focus:ring-secondary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full">Sign Up</button>
-                </div>
-            </form>
-        </div>
+          <div className='col-span-1'>
+            <label
+              htmlFor='role'
+              className='mb-2 block text-sm font-medium text-gray-900'
+            >
+              Register As
+            </label>
+            <select
+              id='role'
+              value={role}
+              onChange={handleRoleChange}
+              className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 shadow-sm focus:border-secondary-600 focus:ring-secondary-600 sm:text-sm'
+            >
+              <option value='STUDENT'>Student</option>
+              <option value='EDUCATOR'>Educator</option>
+            </select>
+          </div>
+
+          <div className='col-span-1'>
+            <button
+              type='submit'
+              className='mt-5 w-full rounded-lg bg-secondary-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-secondary-700 focus:ring-4 focus:ring-secondary-200'
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
     </>
-    )
+  );
 }
--- src/components/SignIn/SignIn.tsx
+++ src/components/SignIn/SignIn.tsx
@@ -1,52 +1,105 @@
-import { useState } from "react";
+import { useEffect, useState } from 'react';
+import { useAppContext } from '../../contexts/AppContext';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignIn() {
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const {
+    state: { user },
+    setUser,
+  } = useAppContext();
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        console.log(`username: ${email}, password: ${password}`);
-    };
+  const nav = useNavigate();
 
-    const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setUsername(e.target.value);
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // TODO: axios request
+    setUser({
+      id: 0,
+      username: username,
+      email: email,
+      password: password,
+      role: 'STUDENT',
+    });
+    console.log(`Logged in as username: ${username}`);
+  };
+
+  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  // TODO: implement logout elsewhere
+  useEffect(() => {
+    setUser(null);
+  }, []);
+
+  useEffect(() => {
+    if (user && user.username) {
+      nav('/dashboard');
     }
+  }, [user, nav]);
 
-    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(e.target.value);
-    };
+  return (
+    <>
+      <div className='flex items-start justify-between rounded-t border-b p-5'>
+        <h3 className='text-xl font-semibold text-gray-900'>Sign In</h3>
+      </div>
 
-    const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(e.target.value);
-    };
+      <div className='space-y-6 p-6'>
+        <form className='grid grid-cols-1 gap-6' onSubmit={handleSubmit}>
+          <div className='col-span-1'>
+            <label
+              htmlFor='username'
+              className='mb-2 block text-sm font-medium text-gray-900'
+            >
+              Username
+            </label>
+            <input
+              id='username'
+              type='text'
+              placeholder='Enter username...'
+              required
+              onChange={handleUsernameChange}
+              className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 shadow-sm focus:border-secondary-600 focus:ring-secondary-600 sm:text-sm'
+            />
+          </div>
 
-    return (
-        <>
-            <div className="flex items-start justify-between p-5 border-b rounded-t">
-                <h3 className="text-xl font-semibold text-gray-900">
-                    Sign In
-                </h3>
-            </div>
+          <div className='col-span-1'>
+            <label
+              htmlFor='password'
+              className='mb-2 block text-sm font-medium text-gray-900'
+            >
+              Password
+            </label>
+            <input
+              id='password'
+              type='password'
+              required
+              onChange={handlePasswordChange}
+              className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 shadow-sm focus:border-secondary-600 focus:ring-secondary-600 sm:text-sm'
+            />
+          </div>
 
-            <div className="p-6 space-y-6">
-                <form className="grid grid-cols-1 gap-6" onSubmit={handleSubmit}>
-                    <div className="col-span-1">
-                        <label htmlFor="username" className="text-sm font-medium text-gray-900 block mb-2">Username</label>
-                        <input id="username" type="text" placeholder="Enter username..." required onChange={handleEmailChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"/>
-                    </div>
-
-                    <div className="col-span-1">
-                        <label htmlFor="password" className="text-sm font-medium text-gray-900 block mb-2">Password</label>
-                        <input id="password" type="password" required onChange={handlePasswordChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5"/>
-                    </div>
-
-                    <div className="col-span-1">
-                        <button type="submit" className="text-white bg-secondary-500 hover:bg-secondary-700 focus:ring-4 focus:ring-secondary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center w-full">Sign In</button>
-                    </div>
-                </form>
-            </div>
-        </>
-    )
+          <div className='col-span-1'>
+            <button
+              type='submit'
+              className='mt-5 w-full rounded-lg bg-secondary-500 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-secondary-700 focus:ring-4 focus:ring-secondary-200'
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
