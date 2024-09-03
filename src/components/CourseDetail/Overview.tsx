8c47e27de08fc8b59d94291ed8777ae8e450f880
dbeauper <beauperthuy.diego@gmail.com>
2024-09-03 14:42:29 -0400

    Individual Couse Page

---
+++ src/components/CourseDetail/Overview.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Overview: React.FC = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold">Course Title</h1>
+            
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquam, nunc id aliquet tincidunt, velit nunc tincidunt nunc, 
+                nec lacinia nunc nisl id nunc. Sed nec nunc non nunc aliquam aliquam. Sed nec nunc non nunc aliquam aliquam.</p>
+        </div>
+    );
+};
+
+export default Overview;
