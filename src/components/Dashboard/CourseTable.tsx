a2a36f8564bc9220be4f5f242ce7788993a8a1c9
MiguelArias01 <Argenis803@gmail.com>
2024-09-05 16:38:01 -0400

    added some cheks

--- src/components/Dashboard/CourseTable.tsx
+++ src/components/Dashboard/CourseTable.tsx
@@ -3,25 +3,26 @@ import axios from 'axios';
 
 interface Course {
   name: string;
-  instructor: string;
-  phone: string;
-  email: string;
-  grade: string;
-  status: string;
+  description: string;
+  attendanceMethod: string;
+  startDate: Date;
+  endDate: Date;
 }
 
 interface CourseTableProps {
   role: string | undefined;
+  id: number | undefined;
   
 }
 
-const CourseTable: React.FC<CourseTableProps> = ({ role }) => {
+const CourseTable: React.FC<CourseTableProps> = ({ role, id }) => {
   const [courses, setCourses] = React.useState<Course[]>([]);
 
   React.useEffect(() => {
     const fetchCourses = async () => {
       try {
-        const response = await axios.get(`http://localhost:8080/api/v1/user/1`);
+        const response = await axios.get(role === 'EDUCATOR' ? `http://localhost:8080/api/v1/course`
+          : `http://localhost:8080/api/v1/user/${id}/enrolledCourses`);
         setCourses(response.data);
         console.log('Courses:', response.data);
       } catch (error) {
@@ -51,29 +52,28 @@ const CourseTable: React.FC<CourseTableProps> = ({ role }) => {
       <table className="min-w-full bg-white">
         <thead>
           <tr>
-            <th className="py-2">Course</th>
-            <th className="py-2">Instructor</th>
-            <th className="py-2">Phone Number</th>
-            <th className="py-2">Email</th>
-            <th className="py-2">Grade</th>
-            <th className="py-2">Status</th>
+            <th className="py-2">Name</th>
+            <th className="py-2">Description</th>
+            <th className="py-">Attendance Method</th>
+            <th className="py-2">StartDate</th>
+            <th className="py-2">EndDate</th>
           </tr>
         </thead>
         <tbody>
           {courses.map((course, index) => (
             <tr key={index} className="text-center">
               <td className="py-2">{course.name}</td>
-              <td className="py-2">{course.instructor}</td>
-              <td className="py-2">{course.phone}</td>
-              <td className="py-2">{course.email}</td>
-              <td className="py-2">{course.grade}</td>
+              <td className="py-2">{course.description}</td>
+              <td className="py-2">{course.attendanceMethod}</td>
+              <td className="py-2">{course.startDate.toString()}</td>
+              <td className="py-2">{course.endDate.toString()}</td>
               <td className="py-2">
                 <span
                   className={`px-2 py-1 rounded-full text-white ${
-                    course.status === 'Active' ? 'bg-green-500' : 'bg-red-500'
+                    course.startDate >= new Date() ? 'bg-green-500' : 'bg-red-500'
                   }`}
                 >
-                  {course.status}
+                  {course.startDate.toString()}
                 </span>
               </td>
             </tr>
