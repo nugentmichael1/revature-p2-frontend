a3ae1ce40ed9efdc88046c2f75c517263d1274a4
dbeauper <beauperthuy.diego@gmail.com>
2024-09-10 16:55:16 -0400

    show students in course

--- src/components/CourseDetail/Students.tsx
+++ src/components/CourseDetail/Students.tsx
@@ -1,14 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useAppContext } from '../../contexts/AppContext';
+
+interface Course {
+    id: number;
+}
+
+interface Student {
+    firstName: string;
+    lastName: string;
+}
 
 const Students: React.FC = () => {
+    const { courseId } = useParams<{ courseId: string }>(); 
+    const [students, setStudents] = useState<Student[]>([]); 
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string | null>(null);
+
+
+    const {
+        state: { user },
+    } = useAppContext();
+
+    useEffect(() => {
+        const fetchStudents = async () => {
+
+            try {
+                
+                console.log('Token:', user?.token);
+
+                const headers = {
+                    headers: {
+                        Authorization: user?.token 
+                    },
+                };
+
+                const response = await axios.get(`${import.meta.env.VITE_API_URL}/course/${courseId}/students`, headers);
+                setStudents(response.data);
+            } catch (err) {
+                setError('No Students Enrolled in Course');
+            }
+        };
+
+        if (courseId && user?.token) {
+            fetchStudents();
+        }
+    }, [courseId, user?.token]); 
+
+    if (error) {
+        return <p>{error}</p>;
+    }
+
     return (
         <div className="min-h-200">
             <h1>Students</h1>
-            <p>Student 1</p>
-            <p>Student 2</p>
-            <p>Student 3</p>
-            <p>Student 4</p>
-            <p>Student 5</p>
+            {students.length > 0 ? (
+                <ul>
+                    {students.map((student, index) => (
+                        <li key={index}>
+                            {student.firstName} {student.lastName}
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No students enrolled in this course.</p>
+            )}
         </div>
     );
 };
