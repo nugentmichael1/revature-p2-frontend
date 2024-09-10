ff478e62b4b5ac23cd53bf1701dd8b71e0752120
dbeauper <beauperthuy.diego@gmail.com>
2024-09-10 16:54:30 -0400

    hide Discussion and Student
    hide discussion and student button if not signed in

--- src/components/CourseDetail/CourseNavBar.tsx
+++ src/components/CourseDetail/CourseNavBar.tsx
@@ -1,12 +1,19 @@
 import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
 import Overview from './Overview';
 import Students from './Students';
 import DiscussionBoard from '../Discussions/DiscussionBoard';
 import CheckoutButton from '../Checkout/CheckoutButton';
+import { useAppContext } from '../../contexts/AppContext';
 
 const CourseNavBar: React.FC = () => {
     const [active, setActive] = useState("Overview");
-    const [isEnrolled, setIsEnrolled] = useState(false);
+    const [isEnrolled] = useState(false);
+    const { courseId } = useParams();
+
+    const {
+        state: { user }, 
+    } = useAppContext();
 
     return (
         <div>
@@ -14,16 +21,19 @@ const CourseNavBar: React.FC = () => {
                 <div className="flex space-x-4">
                     <nav>
                         <button onClick={() => setActive("Overview")} className="px-4 py-2 bg-blue-500 text-white rounded">Overview</button>
+                        {user && ( 
                         <button onClick={() => setActive("Discussion")} className="px-4 py-2 bg-red-500 text-white rounded">Discussion</button>
+                        )}
+                        {user && (
                         <button onClick={() => setActive("Students")} className="px-4 py-2 bg-green-500 text-white rounded">Students</button>
-                        {!isEnrolled && <CheckoutButton />}
+                        )}
+                        {!isEnrolled && courseId && <CheckoutButton courseId={Number(courseId)}/>}
                     </nav>
                 </div>
             </div>
             <div className="flex justify-between items-center bg-gray-200 p-4">                               
                 {active === "Overview" && <Overview />}
-                {/* TODO: pass actual courseId as prop */}
-                {active === "Discussion" && <DiscussionBoard courseId={1}/>}
+                {active === "Discussion" && courseId && <DiscussionBoard courseId={Number(courseId)}/>}
                 {active === "Students" && <Students />}                  
             </div>
         </div>
