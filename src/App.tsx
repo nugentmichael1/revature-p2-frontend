246f49496f93610ff8e31395b3176ffa1c5aec70
Trey Crossley <treycrossley@gmail.com>
2024-09-10 10:33:33 -0700

    app main refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './pages/Layout/layout';
+import CreateCourse from './pages/CreateCourse/CreateCourse';
+import SignInRegister from './pages/SignInRegisterPage/SignInRegisterPage';
+import { AppProvider } from './contexts/AppContext';
+import Home from './components/Home/Home';
+import DashboardPage from './pages/Dashboard/DashboardPage';
+import AllCourses from './pages/AllCourses/AllCourses';
+import Exam from './pages/Exam/Exam';
+import CoursePage from './pages/Course/CoursePage';
+import ContactUs from './pages/ContactUs/ContactUs';
+import AboutUs from './pages/AboutUs/AboutUs';
+import CreateExam from './pages/CreateExam/CreateExam';
+import CheckoutSuccess from './pages/Checkout/CheckoutSuccess';
+import CheckoutCancel from './pages/Checkout/CheckoutCancel';
+import Profile from './pages/Profile/Profile';
+import CourseBoard from './pages/CourseBoard/CourseBoard';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <AppProvider>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='/register' element={<SignInRegister />} />
+            <Route path='/login' element={<SignInRegister />} />
+            <Route path='/dashboard' element={<DashboardPage />} />
+            <Route path='/createcourse' element={<CreateCourse />} />
+            <Route path='/courses' element={<AllCourses />} />
+            <Route path='/exam/:examId' element={<Exam />} />
+            <Route path='/createexam' element={<CreateExam moduleId={0} />} />
+            <Route path='/course/:courseId' element={<CoursePage />} />
+            <Route path='/checkout-success' element={<CheckoutSuccess />} />
+            <Route path='/checkout-cancel' element={<CheckoutCancel />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route
+              path='/dashboard/courses/:courseId'
+              element={<CourseBoard />}
+            />
+            <Route path='/contactus' element={<ContactUs />} />
+            <Route path='/aboutus' element={<AboutUs />} />
+          </Route>
+        </Routes>
+      </AppProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,51 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
-import Layout from '../src/pages/Layout/layout';
-import CreateCourse from './pages/CreateCourse/CreateCourse';
-import SignInRegister from './pages/SignInRegisterPage/SignInRegisterPage';
-import { AppProvider } from './contexts/AppContext';
-import Home from './components/Home/Home';
-import DashboardPage from './pages/Dashboard/DashboardPage';
-import AllCourses from './pages/AllCourses/AllCourses';
-import Exam from './pages/Exam/Exam';
-import CoursePage from './pages/Course/CoursePage';
-import ContactUs from './pages/ContactUs/ContactUs';
-import AboutUs from './pages/AboutUs/AboutUs';
-import CreateExam from './pages/CreateExam/CreateExam';
-import CheckoutSuccess from './pages/Checkout/CheckoutSuccess';
-import CheckoutCancel from './pages/Checkout/CheckoutCancel';
-import Profile from './pages/Profile/Profile';
-import CourseBoard from './pages/CourseBoard/CourseBoard';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppProvider>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path='/register' element={<SignInRegister />} />
-            <Route path='/login' element={<SignInRegister />} />
-            <Route path='/dashboard' element={<DashboardPage />} />
-            <Route path='/createcourse' element={<CreateCourse />} />
-            <Route path='/courses' element={<AllCourses />} />
-            <Route path='/exam/:examId' element={<Exam />} />
-            <Route path='/createexam' element={<CreateExam moduleId={0} />} />
-            <Route path='/course/:courseId' element={<CoursePage />} />
-            <Route path='/checkout-success' element={<CheckoutSuccess />} />
-            <Route path='/checkout-cancel' element={<CheckoutCancel />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route
-              path='/dashboard/courses/:courseId'
-              element={<CourseBoard />}
-            />
-            <Route path='/contactus' element={<ContactUs />} />
-            <Route path='/aboutus' element={<AboutUs />} />
-          </Route>
-        </Routes>
-      </AppProvider>
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 );
