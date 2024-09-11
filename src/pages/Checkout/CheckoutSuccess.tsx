39181d6911639a18013367f1e3719b05a6b9ab1a
Pichborith Kong <pichborith@gmail.com>
2024-09-11 12:13:45 -0700

    fixed check mark image

--- src/pages/Checkout/CheckoutSuccess.tsx
+++ src/pages/Checkout/CheckoutSuccess.tsx
@@ -1,22 +1,18 @@
 import React from 'react';
-
+import checkmark from '../../assets/checkmark.png';
 
 const CheckoutSuccess: React.FC = () => {
-
-
-    return (
-        <>
-
-        <div className="min-h-screen flex flex-col items-center bg-gray-50">
-            <h1 className="mt-20 mb-8 text-4xl font-bold flex items-center">
-                <img src="/src/assets/checkmark.png" alt="success" className="w-12 h-12 mr-2"></img> 
-                Transaction Successful!
-            </h1>
-            <p className="text-lg mb-20">Thank you for supporting RevLearn!</p>
-        </div>
-
-        </>
-    );
+  return (
+    <>
+      <div className='flex flex-col items-center bg-gray-50'>
+        <h1 className='mb-8 mt-20 flex items-center text-4xl font-bold'>
+          <img src={checkmark} alt='success' className='mr-2 h-12 w-12'></img>
+          Transaction Successful!
+        </h1>
+        <p className='mb-20 text-lg'>Thank you for supporting RevLearn!</p>
+      </div>
+    </>
+  );
 };
 
 export default CheckoutSuccess;
