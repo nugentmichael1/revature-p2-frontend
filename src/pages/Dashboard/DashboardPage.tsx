bef8d921d62f843f6762c6903978dec6d80a7ab7
Bo <bbonning42@gmail.com>
2024-09-12 01:21:17 -0500

    create course removed for students

--- src/pages/Dashboard/DashboardPage.tsx
+++ src/pages/Dashboard/DashboardPage.tsx
@@ -9,43 +9,47 @@ const DashboardPage: React.FC = () => {
   const { state } = useAppContext();
   const userRole = state.user?.role;
   const userId = state.user?.id;
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
   // console.log(state.user);
 
   useEffect(() => {
     if (!userRole) {
-      navigate('/'); 
+      navigate('/');
     }
   }, [userRole, navigate]);
 
   if (!userRole) {
-    return null; 
+    return null;
   }
-  
+
   const handleClick = () => {
-      navigate('/createcourse');
-  }
- 
+    navigate('/createcourse');
+  };
 
   return (
-    <div className="flex">
-      <LeftSidebar firstName={state.user?.firstName} lastName={state.user?.lastName} role={ userRole}/>
-      <div className="flex-1 p-6">
+    <div className='flex'>
+      <LeftSidebar
+        firstName={state.user?.firstName}
+        lastName={state.user?.lastName}
+        role={userRole}
+      />
+      <div className='flex-1 p-6'>
         <Dashboard />
         <CourseTable role={userRole} id={userId} />
-        
-        
-        <div className="text-right mt-4">
-          <button 
-            onClick={handleClick} 
-            className="px-5 py-2 bg-orange-500 text-white rounded hover:bg-sky-500"
-          >
-            Create Course
-          </button>
-        </div>
+
+        {userRole !== 'STUDENT' && (
+          <div className='mt-4 text-right'>
+            <button
+              onClick={handleClick}
+              className='rounded bg-orange-500 px-5 py-2 text-white hover:bg-sky-500'
+            >
+              Create Course
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default DashboardPage;
